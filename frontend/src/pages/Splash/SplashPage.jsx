bf1b5f2e253c850af2476c4fd3bcb170766bce39
2sseul <iseul0626@gmail.com>
2026-02-01 14:02:24 +0900

    feat: splash 페이지 수정

--- frontend/src/pages/Splash/SplashPage.jsx
+++ frontend/src/pages/Splash/SplashPage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Button from "@/components/Button";
+import "./SplashPage.css";
 
 export default function SplashPage() {
   const navigate = useNavigate();
@@ -8,11 +8,42 @@ export default function SplashPage() {
     navigate("/home");
   };
 
+  const goNaverLogin = () => {
+    // TODO: 네이버 로그인 구현
+  };
+
   return (
     <div className="splash-container">
-      <div className="splash-content">
-        <Button onClick={goHome}>로그인 없이 사용해보기</Button>
+      {/* RecipeU */}
+      <p className="splash-recipeu">RecipeU</p>
+
+      {/* 레시퓨 */}
+      <div className="splash-title-row">
+        <span className="splash-title-char splash-title-char--reo">레</span>
+        <span className="splash-title-char splash-title-char--si">시</span>
+        <span className="splash-title-char splash-title-char--pyu">퓨</span>
       </div>
+
+      {/* 캐릭터 이미지 */}
+      <img
+        src="/splash-potato.png"
+        alt="레시퓨 캐릭터"
+        className="splash-character-img"
+      />
+
+      {/* 네이버 로그인 */}
+      <button className="splash-naver-btn" onClick={goNaverLogin}>
+        <img
+          src="/login-naver.png"
+          alt="네이버 로그인"
+          className="splash-naver-btn-img"
+        />
+      </button>
+
+      {/* 로그인 없이 사용해보기 */}
+      <button className="splash-guest-btn" onClick={goHome}>
+        로그인 없이 사용해보기
+      </button>
     </div>
   );
 }
