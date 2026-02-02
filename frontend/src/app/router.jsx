b81f86141e52852ae6a4a8118cfdffe6ecfdc1a5
YunHui Kim <yuni2821@naver.com>
2026-02-02 13:56:01 +0900

    fix: resolve JSX syntax error

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -6,13 +6,12 @@ import HomePage from "@/pages/Home/HomePage";
 import ChatPage from "@/pages/Chat/ChatPage";
 import LoadingPage from "@/pages/Loading/LoadingPage";
 import RecipeResultPage from "@/pages/Recipes/RecipeResultPage";
-// import RecipeDetailPage from "@/pages/Recipes/RecipeDetailPage";
 import CookModePage from "@/pages/Cook/CookModePage";
 import CookModeAudioPage from "@/pages/Cook/CookModeAudioPage";
 import MyRecipesPage from "@/pages/MyRecipes/MyRecipesPage";
 import CookStartPage from "@/pages/Cook/CookStartPage";
-// import CookCompletePage from "@/pages/Cook/CookCompletePage";
 import MyPage from "@/pages/MyPages/MyPage";
+
 import FixedLayout from "@/layouts/FixedLayout";
 import ScrollLayout from "@/layouts/ScrollLayout";
 import MobileLayout from "@/layouts/MobileLayout";
@@ -20,98 +19,88 @@ import MobileLayout from "@/layouts/MobileLayout";
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Splash - 고정 화면 */}
-        <Route
-          path="/"
-          element={
-            <FixedLayout>
-              <SplashPage />
-            </FixedLayout>
-          }
-        />
+      <MobileLayout>
+        <Routes>
+          {/* Splash - 고정 화면 */}
+          <Route
+            path="/"
+            element={
+              <FixedLayout>
+                <SplashPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Home - 스크롤 화면 */}
-        <Route
-          path="/home"
-          element={
-            <ScrollLayout>
-              <HomePage />
-            </ScrollLayout>
-          }
-        />
+          {/* Home - 스크롤 화면 */}
+          <Route
+            path="/home"
+            element={
+              <ScrollLayout>
+                <HomePage />
+              </ScrollLayout>
+            }
+          />
 
-        {/* MyPage - 알레르기, 조리 기구 정보 */}
-        <Route
-          path="/mypage"
-          element={
-            <FixedLayout>
-              <MyPage />
-            </FixedLayout>
-          }
-        />
+          {/* MyPage - 알레르기, 조리 기구 정보 */}
+          <Route
+            path="/mypage"
+            element={
+              <FixedLayout>
+                <MyPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Chat - 고정 화면 (입력창 고정) */}
-        <Route
-          path="/chat"
-          element={
-            <FixedLayout>
-              <ChatPage />
-            </FixedLayout>
-          }
-        />
+          {/* Chat - 고정 화면 (입력창 고정) */}
+          <Route
+            path="/chat"
+            element={
+              <FixedLayout>
+                <ChatPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Loading - 고정 화면 (로딩 애니메이션) */}
-        <Route
-          path="/loading"
-          element={
-            <FixedLayout>
-              <LoadingPage />
-            </FixedLayout>
-          }
-        />
+          {/* Loading - 고정 화면 (로딩 애니메이션) */}
+          <Route
+            path="/loading"
+            element={
+              <FixedLayout>
+                <LoadingPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Recipe Result - 고정 화면 (모달형) */}
-        <Route
-          path="/recipe-result"
-          element={
-            <FixedLayout>
-              <RecipeResultPage />
-            </FixedLayout>
-          }
-        />
+          {/* Recipe Result - 고정 화면 (모달형) */}
+          <Route
+            path="/recipe-result"
+            element={
+              <FixedLayout>
+                <RecipeResultPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Cook Mode - 고정 화면 (음성 컨트롤) */}
-        <Route
-          path="/cook-start"
-          element={
-            <FixedLayout>
-              <CookStartPage />
-            </FixedLayout>
-          }
-        />
+          {/* Cook Start */}
+          <Route
+            path="/cook-start"
+            element={
+              <FixedLayout>
+                <CookStartPage />
+              </FixedLayout>
+            }
+          />
 
-        {/* Cook Mode - 고정 화면 (음성 컨트롤) */}
-        <Route
-          path="/cook"
-          element={
-            <FixedLayout>
-              <CookModePage />
-            </FixedLayout>
-          }
-        />
+          {/* Cook Mode */}
+          <Route
+            path="/cook"
+            element={
+              <FixedLayout>
+                <CookModePage />
+              </FixedLayout>
+            }
+          />
 
-        {/* 마이 레시피 - 고정 화면 (내부 스크롤) */}
-        <Route
-          path="/recipes/my"
-          element={
-            <FixedLayout>
-              <MyRecipesPage />
-            </FixedLayout>
-          }
-        />
-
-<<<<<<< HEAD
           {/* Cook Mode Audio - 음성 녹음 페이지 */}
           <Route
             path="/cook-audio"
@@ -122,14 +111,13 @@ export default function Router() {
             }
           />
 
-          {/* 마이 레시피 - 스크롤 화면 */}
+          {/* 마이 레시피 - 고정 화면 (내부 스크롤) */}
           <Route
             path="/recipes/my"
             element={
-              <ScrollLayout>
-                {/* TODO: MyRecipesPage 컴포넌트 생성 */}
-                <div>마이 레시피 페이지</div>
-              </ScrollLayout>
+              <FixedLayout>
+                <MyRecipesPage />
+              </FixedLayout>
             }
           />
 
@@ -138,26 +126,12 @@ export default function Router() {
             path="/recipes"
             element={
               <ScrollLayout>
-                {/* TODO: AllRecipesPage 컴포넌트 생성 */}
                 <div>전체 레시피 페이지</div>
               </ScrollLayout>
             }
           />
         </Routes>
       </MobileLayout>
-=======
-        {/* 전체 레시피 - 스크롤 화면 */}
-        <Route
-          path="/recipes"
-          element={
-            <ScrollLayout>
-              {/* TODO: AllRecipesPage 컴포넌트 생성 */}
-              <div>전체 레시피 페이지</div>
-            </ScrollLayout>
-          }
-        />
-      </Routes>
->>>>>>> b5e4f8bc8b740bc60b5ed2b686775d8a797f4330
     </BrowserRouter>
   );
 }
