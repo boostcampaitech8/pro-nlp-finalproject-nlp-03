5d3ff99eba0bcfb372cb91863dce6ecd824d3bd8
2sseul <iseul0626@gmail.com>
2026-02-04 22:41:04 +0900

    fix: backend 디렉토리 delete

--- backend/services/__init__.py
+++
@@ -1,9 +0,0 @@
-# backend/services/__init__.py
-
-from .search import get_search_service, NaverBlogSearch, GoogleCustomSearch
-
-__all__ = [
-    'get_search_service',
-    'NaverBlogSearch',
-    'GoogleCustomSearch'
-]
